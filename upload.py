5a1ffcc81dbe0e01de9ae15e3d0e13cc20ffc214
Falbue <cyansair05@gmail.com>
2025-02-27 03:38:35 +0500

    исправлена ошибка

--- upload.py
+++ upload.py
@@ -51,6 +51,7 @@ def send_schedule():
                 group_url = groups[group]
                 schedule = json.loads(parser.get_schedule(group_url))
                 data[group] = schedule
+                date, time = now_time()
                 print(f"[ {date} {time} ]    Группа {group} получена...")
     response = requests.post('https://falpin.ru/api/save_schedule', json=data, headers=headers)
     date, time = now_time()
